b887b5f37863fee987f7ba31e7b8b3ead11b915c
AravinthJ20 <156094781+AravinthJ20@users.noreply.github.com>
2026-06-19 10:15:19 +0530

    Server Port changed to 5000

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res) => {
   res.status(404).render('404', { title: 'Page not found' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
